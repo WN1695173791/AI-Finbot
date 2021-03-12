bf71f40ed170632ffd1023e1aa6cfc18ef0640b5
Akib <akibmashrur@gmail.com>
2021-03-12 21:41:58 +1100

    Links to example secret key and dependencies

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
 from bot import Bot
+import settings
 
 if __name__ == "__main__":
     bot = Bot("Arya")
     bot.hello()
+    key = settings.SECRET_KEY
+    print(key)
 
